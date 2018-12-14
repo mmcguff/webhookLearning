d7eedd0657a902dbd7624656b5ffcb8027dae441
Matthew McGuff <matthewmcguff85@gmail.com>
2018-12-14 13:08:51 -0600

    full crud operations in place

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {User, validate} = require('../models/user');
 const _ = require('lodash');
 
-//POST
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
@@ -16,14 +15,39 @@ router.post('/', async (req, res) => {
     res.send(_.pick(user, ['_id', 'username']));
 });
 
-//GET
 router.get('/', async (req, res) =>{
     const users = await User.find().select('-password');
     res.send(users);
 });
-//PUT 
 
-//DELETE
+router.get('/:id',  async (req, res) => {
 
+    const user = await User.findById(req.params.id).select('-password');
+
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+    res.send(user);
+});
+
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const user = await User.findByIdAndUpdate(req.params.id, {username: req.body.username, password: req.body.password}, {
+      new: true
+    }).select('-password');
+  
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+    
+    res.send(user);
+  });
+
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id).select('-password');
+  
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+  
+    res.send(user);
+  });
 
 module.exports = router;
