c04cf5a42a1b704d73e06419da4aba6244ebbc79
Matthew McGuff <matthew.mcguff@walmart.com>
2019-01-15 15:20:59 -0600

    add basic route for webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,5 +2,25 @@ const express = require('express');
 const router = express.Router();
 const {Webhook, validate} = require('../models/webhook');
 const _ = require('lodash');
-const rp = require('request-promise');
 
+router.post('/', async (req, res) => {
+    res.send('Webhook Post Request');
+});
+
+router.get('/', async (req, res) =>{
+    res.send('Webhook Get All Request');
+});
+
+router.get('/:id',  async (req, res) => {
+    res.send('Webhook Get by id Request');
+});
+
+router.put('/:id', async (req, res) => {
+    res.send('Webhook PUT update Request');
+  });
+
+router.delete('/:id', async (req, res) => {
+    res.send('Webhook Delete Request');
+  });
+
+module.exports = router;
